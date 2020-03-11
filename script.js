cc89ff9064577b4059f8a155b6fbc877742efd60
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-11 11:26:17 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// TODO: Write code so the generatePassword returns a string for a password
+// which meets the requirements in the instructions.
+function generatePassword() {
+  return "";
+}
